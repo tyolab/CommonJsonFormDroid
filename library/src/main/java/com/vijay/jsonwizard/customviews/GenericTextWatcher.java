f36fc1371985038644b34a242cfbc34668d4c3ed
tyodev <deve@tyo.com.au>
2017-10-30 14:26:38 +1000

    binding the json form library with CommonAppLib

--- library/src/main/java/com/vijay/jsonwizard/customviews/GenericTextWatcher.java
+++ library/src/main/java/com/vijay/jsonwizard/customviews/GenericTextWatcher.java
@@ -1,14 +1,18 @@
 package com.vijay.jsonwizard.customviews;
 
-import android.support.v7.internal.widget.TintContextWrapper;
+import android.support.v7.widget.TintContextWrapper;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 
 import com.vijay.jsonwizard.R;
 import com.vijay.jsonwizard.interfaces.JsonApi;
 
 import org.json.JSONException;
+
+import au.com.tyo.app.CommonApp;
+import au.com.tyo.app.Controller;
 
 public class GenericTextWatcher implements TextWatcher {
 
@@ -27,23 +31,39 @@ public class GenericTextWatcher implements TextWatcher {
     }
 
     public void afterTextChanged(Editable editable) {
-        String text = editable.toString();
-        JsonApi api = null;
-        if(mView.getContext() instanceof JsonApi) {
-            api = (JsonApi) mView.getContext();
-        } else if(mView.getContext() instanceof TintContextWrapper) {
-            TintContextWrapper tintContextWrapper = (TintContextWrapper) mView.getContext();
-            api = (JsonApi) tintContextWrapper.getBaseContext();
-        } else {
-            throw new RuntimeException("Could not fetch context");
+        try {
+            String text = editable.toString().trim();
+
+            if (text.length() > 0) {
+                JsonApi api = null;
+
+                String key = (String) mView.getTag(R.id.key);
+
+                Controller controller = (Controller) CommonApp.getInstance();
+                if (controller.getUi().getCurrentPage() instanceof JsonApi) {
+                    api = (JsonApi) controller.getUi().getCurrentPage();
+                }
+                else if (mView.getContext() instanceof JsonApi) {
+                    api = (JsonApi) mView.getContext();
+                }
+                else if (mView.getContext() instanceof TintContextWrapper) {
+                    TintContextWrapper tintContextWrapper = (TintContextWrapper) mView.getContext();
+                    api = (JsonApi) tintContextWrapper.getBaseContext();
+                }
+                else {
+                    throw new RuntimeException("Could not fetch context");
+                }
+
+                try {
+                    api.writeValue(mStepName, key, text);
+                } catch (JSONException e) {
+                    // TODO- handle
+                    e.printStackTrace();
+                }
+            }
+        } catch (Exception e) {
+            Log.e("GenericTextWatcher", "Error in catching text edit value");
         }
 
-        String key = (String) mView.getTag(R.id.key);
-        try {
-            api.writeValue(mStepName, key, text);
-        } catch (JSONException e) {
-            // TODO- handle
-            e.printStackTrace();
-        }
     }
 }
